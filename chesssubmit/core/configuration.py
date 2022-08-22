92e5f8aedd0b30d725d3082ff424b476e58f2fc4
Roger Marsh <roger.marsh@solentware.co.uk>
2022-08-22 08:33:32 +0000

    Change Configuration class to customise Configuration class from solentware_misc project.

--- chesssubmit/core/configuration.py
+++ chesssubmit/core/configuration.py
@@ -2,25 +2,31 @@
 # Copyright 2022 Roger Marsh
 # Licence: See LICENCE (BSD licence)
 
-"""Access and update names of last used configuration files.
+"""Access and update items in a configuration file.
 
-The initial values are taken from file named in self._RESULTS_CONF in the
+The initial values are taken from file named in self._CONFIGURATION in the
 user's home directory if the file exists.
 
 """
+from solentware_misc.core import configuration
 
-from chessvalidate.core import configuration
+import ecfformat.core.constants
 
-from ..core import constants
+from . import constants
 
 
 class Configuration(configuration.Configuration):
     """Identify configuration and recent files and delegate to superclass."""
 
-    _RESULTS_CONF = ".chesssubmit.conf"
-    _DEFAULT_RECENTS = (
+    _CONFIGURATION = ".chesssubmit.conf"
+    _DEFAULT_ITEM_VAULES = (
         (constants.RECENT_EMAIL_SELECTION, "~"),
         (constants.RECENT_EMAIL_EXTRACTION, "~"),
         (constants.RECENT_DOCUMENT, "~"),
         (constants.RECENT_SOURCE_SUBMISSION, "~"),
+        (ecfformat.core.constants.RECENT_RESULTS_FORMAT_FILE, "~"),
+        (
+            ecfformat.core.constants.SHOW_VALUE_BOUNDARY,
+            ecfformat.core.constants.SHOW_VALUE_BOUNDARY_TRUE,
+        ),
     )
